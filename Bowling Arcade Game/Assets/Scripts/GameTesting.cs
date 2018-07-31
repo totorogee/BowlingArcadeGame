de03491f8091fa85d97cfe81d126f3b539a60a60
Yeung Pak Sum <paksumy@animocabrands.com>
2018-07-31 23:48:24 +0800

    [UPDATE] Add Game Testing script to shoot testing ball with force setting

---
+++ Bowling Arcade Game/Assets/Scripts/GameTesting.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTesting : MonoBehaviour {
+
+	public bool TestShootTrigger = false;
+	public Transform TestShootStartPoint;
+	public GameObject BallPrefabs;
+
+	public Vector3 Force;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(TestShootTrigger){
+			TestShootTrigger = false;
+			TestShoot();
+		}
+	}
+
+	void TestShoot(){
+		GameObject ball = Instantiate(BallPrefabs, TestShootStartPoint.position, Quaternion.identity);
+		Rigidbody rb = null;
+		if (ball.GetComponent<Rigidbody>() != null){
+			rb = ball.GetComponent<Rigidbody>();
+		}
+		else{
+			rb =  ball.AddComponent<Rigidbody>();
+		}
+
+		rb.AddForce(Force, ForceMode.Impulse);
+
+
+	}
+}
