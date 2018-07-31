0ee8eda792fd0eefb2cfde557d3a8a5f476c66b4
Yeung Pak Sum <paksumy@animocabrands.com>
2018-07-31 23:47:05 +0800

    [UPDATE] Add Bounciness to ball; Add Target script to detect hits and add scores

---
+++ Bowling Arcade Game/Assets/Scripts/Target.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+	[SerializeField] private int score = 100;
+    
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log("hit : " + collision.collider.tag);
+		GameController.Instance.TotalScore += score;
+	}
+
+}
