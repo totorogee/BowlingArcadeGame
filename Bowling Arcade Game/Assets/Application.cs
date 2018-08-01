012bea568a867a75b81449d28dfb0143538a4b1c
Yeung Pak Sum <paksumy@animocabrands.com>
2018-08-01 21:40:50 +0800

    [UPDATE] Add function for UI button on open scene; Add Application script to control changing scene

---
+++ Bowling Arcade Game/Assets/Application.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Application : MonoBehaviour {
+
+	public static Application Instance;
+
+	private void Awake()
+	{
+		Instance = this;
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void LoadScene(int number)
+    {
+		if (SceneManager.sceneCount < number -1)
+		{
+			Debug.LogWarning("Worng Scene Number :" + number);
+			return;
+		}
+		SceneManager.LoadScene(number, LoadSceneMode.Single);
+    }
+}
---
+++ Bowling Arcade Game/Assets/OpeningSceneUI.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class OpeningSceneUI : MonoBehaviour {
+
+	[SerializeField] private Transform leaderBoard;
+
+	[SerializeField] private Button leaderBoardButton;
+	[SerializeField] private Button startGameButton;
+	[SerializeField] private Button leaderBoardBackButton;
+
+	private void OnEnable()
+	{
+		leaderBoardButton.onClick.AddListener(OpenLeaderBoard);
+		startGameButton.onClick.AddListener(StartGame);
+		leaderBoardBackButton.onClick.AddListener(CloseLeaderBoard);
+	}
+
+	private void OnDisable()
+    {
+		leaderBoardButton.onClick.RemoveAllListeners();
+		startGameButton.onClick.RemoveAllListeners();
+		leaderBoardBackButton.onClick.RemoveAllListeners();
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OpenLeaderBoard()
+	{
+		leaderBoard.gameObject.SetActive(true);
+	}
+
+	void StartGame()
+    {
+		Application.Instance.LoadScene(1);
+    }
+
+	void CloseLeaderBoard()
+    {
+		leaderBoard.gameObject.SetActive(false);
+    }
+}
