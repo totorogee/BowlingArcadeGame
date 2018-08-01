7cb3ccabb1003eec3d095c31a7822848bec31934
Yeung Pak Sum <paksumy@animocabrands.com>
2018-08-01 22:52:37 +0800

    [UPDATE] Add UI controller script for Main scene

---
+++ Bowling Arcade Game/Assets/Scripts/Screen/MainSceneUI.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainSceneUI : MonoBehaviour {
+    
+	[SerializeField] private Transform countDownScreen;
+	[SerializeField] private Text countDownText;
+    
+	[SerializeField] private Text inGameScore;
+    [SerializeField] private Text inGameTime;
+
+	[SerializeField] private Transform endGamePopup;
+	[SerializeField] private Text endGameScore;
+	[SerializeField] private Text endGameRank;
+	[SerializeField] private Text endGameText;
+    
+	[SerializeField] private Button endGameRestartButton;
+	[SerializeField] private Button endGameMainMenuButton;
+
+	private int startCounter;
+	private float startingTime;
+	private bool gameReady = false;
+
+
+	private void OnEnable()
+	{
+
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		startingTime = Time.time;
+		startCounter = 3;
+        countDownText.text = startCounter.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (startCounter >=0)
+		{
+			countDownText.text = startCounter.ToString();
+			startCounter = 3 - Mathf.FloorToInt(Time.time - startingTime);
+
+		}
+		else
+		{
+			gameReady = true;
+			GameTesting.Instance.GameStart = true;
+			countDownScreen.gameObject.SetActive(false);
+		}
+
+	}
+}
