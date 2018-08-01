d441c8aeeae1aac2693e5c70e271ad4254acc7a5
Yeung Pak Sum <paksumy@animocabrands.com>
2018-08-01 19:45:57 +0800

    [UPDATE] Add ball script to destroy ball object; GameTesting script to shoot ball by touch screen

---
+++ Bowling Arcade Game/Assets/Scripts/Ball.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+	private const float destoryTime = 5;
+	private float counter = 0;
+	private bool destoryTiggered = false;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(destoryTiggered && Time.time > counter)
+		{
+			destoryTiggered = false;
+			Destroy(this.gameObject);
+		}
+	}
+
+	public void DelayDestory()
+	{
+		counter = destoryTime + Time.time;
+		destoryTiggered = true;
+	}
+
+	private void OnCollisionEnter(Collision collision)
+    {
+		DelayDestory();
+    }
+
+}
--- Bowling Arcade Game/Assets/Scripts/GameTesting.cs
+++ Bowling Arcade Game/Assets/Scripts/GameTesting.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameTesting : MonoBehaviour {
 
@@ -15,13 +16,35 @@ public class GameTesting : MonoBehaviour {
 	void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if(TestShootTrigger){
 			TestShootTrigger = false;
 			TestShoot();
 		}
+        
+		if(Input.GetMouseButtonUp(0)){
+			Debug.Log(ReturnClickedObject().name);
+		}
+
+	}
+
+
+	void TestShoot(Vector3 start )
+	{ 
+		GameObject ball = Instantiate(BallPrefabs, start, Quaternion.identity);
+        Rigidbody rb = null;
+        if (ball.GetComponent<Rigidbody>() != null)
+        {
+            rb = ball.GetComponent<Rigidbody>();
+        }
+        else
+        {
+            rb = ball.AddComponent<Rigidbody>();
+        }
+
+        rb.AddForce(Force, ForceMode.Impulse);
 	}
 
 	void TestShoot(){
@@ -35,7 +58,28 @@ public class GameTesting : MonoBehaviour {
 		}
 
 		rb.AddForce(Force, ForceMode.Impulse);
-
-
 	}
+
+	public GameObject ReturnClickedObject()
+    {
+        RaycastHit hit;
+        GameObject target = null;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray.origin, ray.direction * 10, out hit))
+        {
+            target = hit.collider.gameObject;
+        }
+
+        if (target != null)
+        {
+			TestShoot(hit.point + new Vector3 (0,1.2f,0));
+        }
+
+		Debug.Log(hit.point);
+
+        return target;
+    }
+
+
+
 }
