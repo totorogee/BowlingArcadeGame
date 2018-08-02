03b59e1400bf06ca84cf8b1b4bcf557b76d0d0c2
Yeung Pak Sum <paksumy@animocabrands.com>
2018-08-02 19:32:24 +0800

    [UPDATE] Add StageData class to store setting from config doc.

---
+++ Bowling Arcade Game/Assets/Scripts/StageData.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageData
+{
+
+	public int ID { get; set; } 
+	public string StageName   { get; set; } 
+	public int TimeAllowed { get; set; } 
+	public float PowerChargeSpeed    { get; set; } 
+	public float BallBounciness { get; set; } 
+	public float Size1   { get; set; } 
+	public int Score1 { get; set; } 
+	public float Size2   { get; set; } 
+	public int Score2 { get; set; } 
+	public float Size3   { get; set; } 
+	public int Score3 { get; set; } 
+	public float Size4   { get; set; } 
+	public int Score4 { get; set; } 
+	public float Size5   { get; set; } 
+	public int Score5 { get; set; } 
+	public float Size6   { get; set; } 
+	public int Score6 { get; set; } 
+	public float Size7   { get; set; } 
+	public int Score7 { get; set; } 
+	public float Size8   { get; set; } 
+	public int Score8 { get; set; } 
+	public float Size9   { get; set; } 
+	public int Score9 { get; set; } 
+	public float Size0   { get; set; } 
+	public int Score0 { get; set; } 
+
+
+}
