cc5d15a138aecd58708c70811153dea4c91e1268
Yeung Pak Sum <paksumy@animocabrands.com>
2018-08-02 14:07:02 +0800

    [DRAFT] Start New Branches : Draft on config game by json file function

---
+++ Bowling Arcade Game/Assets/StageSetting.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft;
+
+
+public class StageSetting : MonoBehaviour {
+
+	[SerializeField] private TextAsset stageConfig;
+	private Dictionary<int, StageData> avaliableIStageDic;
+
+	public List<StageData> Datas =new List<StageData>();
+
+	// Use this for initialization
+	void Start () {
+		Datas = GetStageDatas();
+		Debug.Log(Datas.Count + " " + Datas[1].Score9);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public List<StageData> GetStageDatas()
+    {
+		if (avaliableIStageDic == null)
+        {
+			Dictionary<string, StageData> dic = JsonConvert.DeserializeObject<Dictionary<string, StageData>>(stageConfig.text);
+			avaliableIStageDic = dic.ConvertKey();
+        }
+		return new List<StageData>(avaliableIStageDic.Values);
+    }
+
+
+}
---
+++ Bowling Arcade Game/Assets/SystemExtensions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System.Linq;
+using System;
+
+public static class SystemExtensions
+{
+    public static Dictionary<int, T> ConvertKey<T>(this Dictionary<string, T> dic)
+    {
+        return dic.ToDictionary(item => int.Parse(item.Key), item => item.Value);
+    }
+ 
+}
